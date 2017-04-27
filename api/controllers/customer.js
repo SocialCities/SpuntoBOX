91fae96087112adabbbc67318619d5aba3acfd3c
Goncalo Margalho <goncalo@Goncalos-MacBook-Pro.local>
2017-04-27 15:27:57 +0200

    customer edit and delete

--- api/controllers/customer.js
+++ api/controllers/customer.js
@@ -3,6 +3,18 @@
 module.exports = {
   path: '/customers',
   actions: {
+    'get /:account/:customerId': [function(req, res, next) {
+      console.log(req.params.customerId)
+      console.log(req.params.account)
+      Model.customers.findOne({account: req.params.account, id: req.params.customerId}).then((customer) => {
+        
+        console.log(customer)
+        console.log('customer ^^')
+        res.send(customer);
+      }).catch((err) => {
+        console.log('customers Error', err);
+      })
+    }],
     'get /:account': [function(req, res, next) {
       Model.customers.find({account: req.params.account}).then((customers) => {
         res.send(customers);
@@ -14,7 +26,28 @@ module.exports = {
       console.log(req.body)
       let customer = req.body;
       customer.account = req.params.account;
-      Model.customers.create(req.body).then((customer) => {
+      Model.customers.create(customer).then((customer) => {
+        console.log('customer')
+        console.log(customer)
+        res.send(customer);
+      }).catch((err) => {
+        console.log('customers Error', err);
+      })
+    }],
+    'put /:account/:customerId': [function(req, res, next) {
+      Model.customers.update({account: req.params.account, id: req.params.customerId}, req.body).then((customer) => {
+        console.log('customer')
+        console.log(customer)
+        res.send(customer);
+      }).catch((err) => {
+        console.log('customers Error', err);
+      })
+    }],
+    'delete /:account/:customerId': [function(req, res, next) {
+
+      let customer = req.body;
+      customer.account = req.params.account;
+      Model.customers.destroy({id: req.params.customerId, account: req.params.account}).then((customer) => {
         console.log('customer')
         console.log(customer)
         res.send(customer);
